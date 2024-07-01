d4c53d2d3d56f5e818db748902ee7238b3e6826a
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-07-02 00:54:19 +0500

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,51 @@
+// date and time in js
+// const mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+// let mydate = new Date(2024,6,7,11,59,87,8,8,8,)
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toJSON());
+// console.log(mydate.getMinutes());
+// console.log(mydate.getDay());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getHours());
+// console.log(mydate.getMonth());
+// console.log(mydate.setMilliseconds());
+// let sunny = new Date("07-07-2024")
+// console.log(sunny);
+// console.log(sunny.toLocaleDateString());
+// console.log(sunny.toLocaleString());
+//  let myts = Date.now()
+// console.log(myts.toLocaleString());
+// console.log(myts.toFixed(4));
+// let mydate = new Date("07-07-2024")
+// gettime used to get milisec values to help compare better//
+// console.log(Date.now());
+
+// let newdate = new Date()
+// console.log(newdate);
+// console.log(newdate.getDate());
+// console.log(newdate.getDay());
+// console.log(newdate.getHours());
+// console.log(newdate.getMinutes());
+// console.log(newdate.getMilliseconds());
+// console.log(newdate.getMonth());
+// console.log(`hello i am sanaiyla and its ${newdate} and its ${newdate.getDay()}nd day and ${newdate.getMinutes()}minutes have passed`);
+// console.log(`hello i am sanaiyla and its ${newdate} and ${newdate.getHours()} have passed`);
+// let newd = new Date(2024,06,07)
+// console.log(newd);
+// console.log(newd.getDate());
+// console.log(newd.toLocaleString());
+// console.log(newd.getSeconds());
+// console.log(`hello its sunny and its${newd} and this many hours${newd.getHours()+20} have passed i am even counting seconds which are ${newd.getSeconds()+30}`);
+// let newdate = new Date()
+console.log(new Date().toLocaleString ( `default` , {
+    weekday: "long",
+}));
+// console.log(newdate.toLocaleString(`default`, {
+//     weekday: "short"}));
